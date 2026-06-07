2b98bee58d76000b975542f288856509064dcb7f
sreejapriya167 <sreeja.priya1234@gmail.com>
2026-06-07 12:19:37 +0530

    Day1

---
+++ 01basicnpx/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1> React day 1</h1>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+   <h1> React day 1 vite
+
+   </h1>
+  )
+}
+
+export default App
